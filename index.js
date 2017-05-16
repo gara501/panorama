04f06f4ba57662aeedca51af3ef8323c6376e235
andres ramirez <gandres.ramirez@gmail.com>
2017-05-15 19:57:22 -0500

    Demo

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 
 'use strict';
 
-// Create viewer.
-var viewer = new Marzipano.Viewer(document.getElementById('pano'));
+function marzi(element, model) {
+  // Create viewer.
+  var viewer = new Marzipano.Viewer(document.getElementById(element));
 
-// Create source.
-var source = Marzipano.ImageUrlSource.fromString(
-  "/images/17_HR-V_Interior_360_o_{f}.jpg"
-);
+  // Create source.
+  switch (model) {
+    case 'pilot':
+      var source = Marzipano.ImageUrlSource.fromString(
+        "/images/16_Pilot_Interior_360_o_{f}.jpg"
+      );
+    break;
+    case 'hrv':
+      var source = Marzipano.ImageUrlSource.fromString(
+        "/images/17_HR-V_Interior_360_o_{f}.jpg"
+      );
+    break;
+    default:
 
-// Create geometry.
-var geometry = new Marzipano.CubeGeometry([{ tileSize: 1024, size: 1024 }]);
+  }
 
-// Create view.
-var limiter = Marzipano.RectilinearView.limit.traditional(4096, 100*Math.PI/180);
-var view = new Marzipano.RectilinearView(null, limiter);
+  // Create geometry.
+  var geometry = new Marzipano.CubeGeometry([{ tileSize: 1024, size: 1024 }]);
 
-// Create scene.
-var scene = viewer.createScene({
-  source: source,
-  geometry: geometry,
-  view: view,
-  pinFirstLevel: true
-});
+  // Create view.
+  var limiter = Marzipano.RectilinearView.limit.traditional(4096, 100*Math.PI/180);
+  var view = new Marzipano.RectilinearView(null, limiter);
 
-// Display scene.
-scene.switchTo();
+  // Create scene.
+  var scene = viewer.createScene({
+    source: source,
+    geometry: geometry,
+    view: view,
+    pinFirstLevel: true
+  });
+
+  // Display scene.
+  scene.switchTo();
+}
+
+marzi('pano', 'pilot');
+marzi('pano2', 'hrv');
--- pan.js
+++ pan.js
@@ -1,16 +1,44 @@
 'use strict';
 
-pannellum.viewer('panorama', {
-  "type": "cubemap",
-  "autoLoad": true,
-  "maxLevel": 10,
-  "showControls": true,
-  "cubeMap": [
-      "/images/17_HR-V_Interior_360_o_0.jpg",
-      "/images/17_HR-V_Interior_360_o_1.jpg",
-      "/images/17_HR-V_Interior_360_o_2.jpg",
-      "/images/17_HR-V_Interior_360_o_3.jpg",
-      "/images/17_HR-V_Interior_360_o_4.jpg",
-      "/images/17_HR-V_Interior_360_o_5.jpg"
-  ]
-});
+function panel(id, model) {
+  console.log(pannellum);
+  switch (model) {
+    case 'hrv':
+      pannellum.viewer(id, {
+        "type": "cubemap",
+        "autoLoad": true,
+        "maxLevel": 10,
+        "showControls": true,
+        "cubeMap": [
+            "/images/17_HR-V_Interior_360_o_0.jpg",
+            "/images/17_HR-V_Interior_360_o_1.jpg",
+            "/images/17_HR-V_Interior_360_o_2.jpg",
+            "/images/17_HR-V_Interior_360_o_3.jpg",
+            "/images/17_HR-V_Interior_360_o_4.jpg",
+            "/images/17_HR-V_Interior_360_o_5.jpg"
+        ]
+      });
+      break;
+    case 'pilot':
+      pannellum.viewer(id, {
+        "type": "cubemap",
+        "autoLoad": true,
+        "maxLevel": 10,
+        "showControls": true,
+        "cubeMap": [
+            "/images/16_Pilot_Interior_360_o_0.jpg",
+            "/images/16_Pilot_Interior_360_o_1.jpg",
+            "/images/16_Pilot_Interior_360_o_2.jpg",
+            "/images/16_Pilot_Interior_360_o_3.jpg",
+            "/images/16_Pilot_Interior_360_o_4.jpg",
+            "/images/16_Pilot_Interior_360_o_5.jpg"
+        ]
+      });
+      break;
+    default:
+  }
+
+}
+
+panel('panorama2', 'hrv');
+panel('panorama', 'pilot');
