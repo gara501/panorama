2e06b7b1be7efe1999e85563d581e03abc1d8c03
Andres Ramirez <andrami1@cobogpgp1372.global.publicisgroupe.net>
2017-05-17 17:10:36 -0500

    Zoom events

--- interior360.js
+++ interior360.js
@@ -1,80 +1,102 @@
-
 var interior360 = (function() {
-  'use strict';
+	'use strict';
+	var marzi = {
+		controls: true,
+		controlsContainer: '',
+		root: '',
+		context: '',
+		image: '',
+		viewer: {},
+		source: {},
+		tileSize: 0,
+		size: 0,
+		zoomLimit: 0,
+	};
 
-  var marzi = {
-    controls: true,
-    controlsContainer: '',
-    root: '',
-    context: '',
-    image: '',
-    viewer: {},
-    source: {},
-    tileSize: 0,
-    size: 0,
-    zoomLimit: 0
-  }
+	function init(options) {
+		marzi.root = document.getElementById(options.root);
+		marzi.context = marzi.root.querySelector('.panorama');
+		marzi.image = options.path + options.image;
+		marzi.viewer = new Marzipano.Viewer(marzi.context);
+		marzi.tileSize = options.tileSize || 1024;
+		marzi.size = options.size || 1024;
+		marzi.zoomLimit = options.zoomLimit || 4096;
+		console.log(marzi.viewer);
+		marzi.zoom = marzi.root.querySelector('.zoom');
+		marzi.zoomIn = marzi.root.querySelector('.zoom-in');
+		marzi.zoomOut = marzi.root.querySelector('.zoom-out');
+		marzi.fullScreen = marzi.root.querySelector('.fullScreen');
+		marzi.controlsContainer = marzi.root.querySelector('.controls');
 
-  function init(options) {
-    marzi.root = document.getElementById(options.root);
-    marzi.context = marzi.root.querySelector('.panorama');
-    marzi.image = options.path + options.image;
-    marzi.viewer = new Marzipano.Viewer(marzi.context);
-    marzi.tileSize = options.tileSize || 1024;
-    marzi.size = options.size || 1024;
-    marzi.zoomLimit = options.zoomLimit || 4096;
-    console.log(marzi.viewer);
-    marzi.zoom = marzi.root.querySelector('.zoom');
-    marzi.fullScreen = marzi.root.querySelector('.fullScreen');
-    marzi.controlsContainer = marzi.root.querySelector('.controls');
+		if (options.controls.length > 0) {
+			var controlsClass = options.controls.split(' ');
+			controlsClass.map(function(el) {
+				marzi.controlsContainer.classList.add(el);
+			});
+		}
 
-    if (!options.controls) {
-      marzi.controlsContainer.classList.add('hide');
-    }
+		marzi.source = Marzipano.ImageUrlSource.fromString(marzi.image);
+		addZoomListener();
+		render();
+	}
 
-    marzi.source = Marzipano.ImageUrlSource.fromString(
-      marzi.image
-    );
-    render();
-  }
+	function addZoomListener() {
+		var velocity = 0.7;
+		var friction = 3;
+		var controls = marzi.viewer.controls();
 
-  function render() {
-    // Create geometry.
-    var geometry = new Marzipano.CubeGeometry([{ tileSize: marzi.tileSize, size: marzi.size }]);
+		controls.registerMethod(
+			'inElement',
+			new Marzipano.ElementPressControlMethod(marzi.zoomIn, 'zoom', -velocity, friction),
+			true
+		);
 
-    // Create view.
-    var limiter = Marzipano.RectilinearView.limit.traditional(marzi.zoomLimit, 100*Math.PI/180);
-    var view = new Marzipano.RectilinearView(null, limiter);
+		controls.registerMethod(
+			'outElement',
+			new Marzipano.ElementPressControlMethod(marzi.zoomOut, 'zoom', velocity, friction),
+			true
+		);
+		console.log('Controles', controls);
+		console.log(event);
+	}
 
-    // Create scene.
-    var scene = marzi.viewer.createScene({
-      source: marzi.source,
-      geometry: geometry,
-      view: view,
-      pinFirstLevel: true
-    });
+	function render() {
+		// Create geometry.
+		var geometry = new Marzipano.CubeGeometry([{ tileSize: marzi.tileSize, size: marzi.size }]);
 
-    // Display scene.
-    scene.switchTo();
-  }
+		// Create view.
+		var limiter = Marzipano.RectilinearView.limit.traditional(marzi.zoomLimit, 100 * Math.PI / 180);
+		var view = new Marzipano.RectilinearView(null, limiter);
 
-  return {
-    init: init
-  }
-}());
+		// Create scene.
+		var scene = marzi.viewer.createScene({
+			source: marzi.source,
+			geometry: geometry,
+			view: view,
+			pinFirstLevel: true,
+		});
+
+		// Display scene.
+		scene.switchTo();
+	}
+
+	return {
+		init: init,
+	};
+})();
 
 var panOptions = {
-  root: 'marzi1',
-  image: '16_Pilot_Interior_360_o_{f}.jpg',
-  path: 'https://gara501.github.io/panorama/images/',
-  controls: true
+	root: 'marzi1',
+	image: '16_Pilot_Interior_360_o_{f}.jpg',
+	path: 'https://gara501.github.io/panorama/images/',
+	controls: 'zoom-desktop-on fullscreen-off zoom-mobile-off',
 };
 
 var panOptionsHrv = {
-  root: 'marzi2',
-  image: '17_HR-V_Interior_360_o_{f}.jpg',
-  path: 'https://gara501.github.io/panorama/images/',
-  controls: false
+	root: 'marzi2',
+	image: '17_HR-V_Interior_360_o_{f}.jpg',
+	path: 'https://gara501.github.io/panorama/images/',
+	controls: '',
 };
 interior360.init(panOptions);
 interior360.init(panOptionsHrv);
